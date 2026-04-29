95310c41f438452f42ae8d62cca1ea4ce076c1f0
Cymm <cymmcymm2@gmail.com>
2026-04-29 15:21:17 +0800

    fixed axios production url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: 'https://study-hub-tq9w.onrender.com/api'
 });
 
-// Add token automatically
 api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
@@ -18,7 +17,6 @@ api.interceptors.request.use(
   error => Promise.reject(error)
 );
 
-// Handle unauthorized access
 api.interceptors.response.use(
   response => response,
   error => {
